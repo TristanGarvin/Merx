3180800cf601d7637998cdd6274e7ac5d25d8a87
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-11-05 22:37:57 -0600

    api route updated

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,6 +4,25 @@ var input = document.querySelector(".game-input");
 var gameName = document.querySelector(".game-name");
 var gameImage = document.querySelector(".game-image");
 
+
+function getGameData() {
+    axios.get(apiUrl + input.value)
+        .then(function (response) {
+            gameName.innerHTML = response.data.forms[0].name;
+            gameImage.src = response.data.sprites.front_default;
+        })
+        .catch(function (error) {
+            gameName.innerHTML = "(An error has occurred.)";
+            gameImage.src = "";
+        });
+}
+
+var button = document.querySelector(".game-button");
+button.addEventListener("click", getGameData);
+
+
+
+
 // const express = require("express");
 // const app = express();
 
@@ -42,25 +61,3 @@ var gameImage = document.querySelector(".game-image");
 //         })
 //     })()
 // })
-
-
-var apiUrl = "https://www.speedrun.com/api/v1/games";
-var input = document.querySelector(".game-input");
-var gameName = document.querySelector(".game-name");
-var gameImage = document.querySelector(".game-image");
-
-function getGameData() {
-    axios.get(apiUrl + input.value)
-        .then(function (response) {
-            gameName.innerHTML = response.data.forms[0].name;
-            gameImage.src = response.data.sprites.front_default;
-        })
-        .catch(function (error) {
-            gameName.innerHTML = "(An error has occurred.)";
-            gameImage.src = "";
-        });
-}
-
-var button = document.querySelector(".game-button");
-button.addEventListener("click", getGameData);
-
