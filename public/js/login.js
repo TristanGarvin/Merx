566fa4384f5c8b4cff231acbfdc6ff219e307a68
Aidan T. Manning <periodicaidan@gmail.com>
2020-11-05 21:19:06 -0600

    Added login form validation

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,30 @@
 const form = document.getElementById('form');
 
-form.addEventListener('submit', e => {
+$('#form').on('submit', e => {
     e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
 
-    fetch('/api/login', {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-    })
-        .then(() => window.location.replace('/home'));
+    ['emailHelp', 'passwordHelp', 'loginHelp']
+        .forEach(val => $(`#${val}`).text(''));
+
+    const email = $('#email').val().trim();
+    const password = $('#password').val().trim();
+
+    if (!email) 
+        $('#emailHelp').text('Your email address is required');
+    
+    if (!password)
+        $('#passwordHelp').text('Your password is required');
+    
+    if ([email, password].some(val => !val))
+        return;
+
+    $.post('/api/login', { email, password })
+        .then(() => window.location.replace('/home'))
+        .catch(err => {
+            console.error(err);
+            if (err.status === 401)
+                $('#loginHelp').text('Incorrect username or password');
+            else
+                $('#loginHelp').text('Error processing your request');
+        });
 });
