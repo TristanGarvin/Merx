a919cf61338e09bd35fb004c88b3d4ee5f6e874a
Aidan T. Manning <periodicaidan@gmail.com>
2020-11-06 12:51:22 -0600

    Leaderboards, almost

--- public/js/home.js
+++ public/js/home.js
@@ -1,18 +1,34 @@
 // const axios = require("axios");
 const apiUrl = "https://www.speedrun.com/api/v1/games?name=";
-const input = $(".game-search").val();
-const gameName = document.querySelector(".game-name");
-const gameImage = document.querySelector(".game-image");
+// const categoryQuery = $('#category-search').val();
+const gameName = $('#gameName');
+const gameImage = $('#gameImg');
 
-$("#search-form").on("submit", getGameData)
+$("#search-form").on("submit", getGameData);
+
 function getGameData(e) {
     e.preventDefault()
-    console.log("searching")
-    $.get(apiUrl + input)
+
+    const gameQuery = $("#game-search").val();
+    $.get(apiUrl + gameQuery)
         .then(function (response) {
             console.log(response)
+            const game = response.data[0];
+            const poster = game.assets['cover-large'].uri;
+            
             // gameName.innerHTML = response.data.games[0].name;
             // gameImage.src = response.data.poster.medium-cover;
+
+            $.get(`https://www.speedrun.com/api/v1/games/${game.id}/categories`)
+                .then(function (categoryData) {
+                    const category = categoryData.data[0];
+
+                    gameName.html(`${game.names.international} (${category.name})`);
+                    gameImage.src(poster);
+
+                    // $.get(category.links.find(link => link.rel === 'leaderboard'))
+                    //     .then()
+                });
         })
         .catch(function (error) {
             gameName.innerHTML = "(An error has occurred.)";
